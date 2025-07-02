bba74959847cc15c50cadf0343d27e64376c3c4c
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-07-02 21:39:04 +0530

    change inital hello wrold path to /

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/leave', leaveRoutes);
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
